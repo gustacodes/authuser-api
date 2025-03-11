2ba6c4dd343b4555c11a45a328fd600a912d5a34
Gustavo Henrique <gustalencarvalho@gmail.com>
2025-03-11 11:44:41 -0300

    feat: implements UserService and controller with http method GET and DELETE

--- src/main/java/com/ead/authuser/services/impl/UserServiceImpl.java
+++ src/main/java/com/ead/authuser/services/impl/UserServiceImpl.java
@@ -1,9 +1,14 @@
 package com.ead.authuser.services.impl;
 
+import com.ead.authuser.models.UserModel;
 import com.ead.authuser.repositories.UserRespository;
 import com.ead.authuser.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -11,5 +16,19 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserRespository userRespository;
 
-    
+
+    @Override
+    public List<UserModel> findAll() {
+        return userRespository.findAll();
+    }
+
+    @Override
+    public Optional<UserModel> findById(UUID userId) {
+        return userRespository.findById(userId);
+    }
+
+    @Override
+    public void delete(UserModel user) {
+        userRespository.delete(user);
+    }
 }
