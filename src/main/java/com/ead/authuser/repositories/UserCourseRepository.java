dd4b7a335f433ac105074dc4f41bc2edc8c400a6
Gustavo Henrique <gustalencarvalho@gmail.com>
2025-03-29 00:04:06 -0300

    feat: implemented verification before delete user

--- src/main/java/com/ead/authuser/repositories/UserCourseRepository.java
+++ src/main/java/com/ead/authuser/repositories/UserCourseRepository.java
@@ -3,9 +3,13 @@ package com.ead.authuser.repositories;
 import com.ead.authuser.models.UserCourseModel;
 import com.ead.authuser.models.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface UserCourseRepository extends JpaRepository<UserCourseModel, UUID> {
     boolean existsByUserAndCourseId(UserModel userModel, UUID courseId);
+    @Query(value = "SEELCT * FROM TB_USERS_COURSES WHERE USER_USER_ID = :userId", nativeQuery = true)
+    List<UserCourseModel> findAllUserCourseIntoUser(UUID userId);
 }
